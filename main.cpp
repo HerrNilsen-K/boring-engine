94b880be8fa4433255ee69017afe9a67a6fd85ba
HerrNilsen-K <typnilsen@gmail.com>
2022-06-29 13:15:04 +0200

    Created the basic structure of the engine

--- main.cpp
+++ main.cpp
@@ -3,20 +3,52 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <memory>
+
+class objects;
 
 class boringEngine {
-public:
-    boringEngine() {
-        std::cout << "boring-engine constructor" << std::endl;
+private:
+    std::vector<objects> objectList;
+
+    void render(std::vector<objects> objectList) {
+
     }
+
+    //TODO Change time type
+    void update(std::vector<objects> objectList, double time) {
+
+    }
+
+//TODO Use proper time type
+    void mainLoop(double deltaTime) {
+
+        update(objectList, deltaTime);
+        render(objectList);
+    }
+
+public:
+    //TODO Create window, create context, etc.
+    bool initEngine() {
+
+        return true;
+    }
+
+    boringEngine() {
+        initEngine();
+        mainLoop(0);
+    }
+
     ~boringEngine() {
         std::cout << "boring-engine destructor" << std::endl;
     }
+
 
 };
 
 
 int main() {
-
-    return EXIT_SUCCESS;
+    std::unique_ptr<boringEngine> engine(new boringEngine());
+    return 0;
 }
